de9a675f68f91faa2ba468a8197386df80fda797
Leslie-N-Cook <leslienichole.co@gmail.com>
2023-05-17 13:00:48 -0500

    documentation

--- Assignments/Program_1/Tile.py
+++ Assignments/Program_1/Tile.py
@@ -7,13 +7,15 @@
 # graphics while playing the sudoku game
 #############################################################
 import pygame
-""" 
-def draw_rect_alpha() : 
-    makes a transparent color filled rectangle 
-"""
-
 
 def draw_rect_alpha(surface, color, rect):
+    """
+        makes a transparent color filled rectangle 
+    Args:
+        surface (surface): pygame display
+        color (RGBA): color of the highlighted rectangle
+        rect (rect): pygame rectangle
+    """
     shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
     pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
     surface.blit(shape_surf, rect)
